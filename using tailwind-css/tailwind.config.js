50cbbcb76c21200dc70d91d05e713e813884ba74
iman <nasreenmalik429@gmail.com>
2023-04-26 12:42:08 +0500

    changes

---
+++ using tailwind-css/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#008000",
+        secondary: "#968888",
+        tertiary: "#151515",
+      },
+    },
+  },
+  plugins: [],
+};

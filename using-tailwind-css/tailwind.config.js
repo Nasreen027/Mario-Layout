27af7dc1734b27f59d6bfda1732d3b98d2514a67
iman <nasreenmalik429@gmail.com>
2023-04-30 17:09:26 +0500

    kk

--- using-tailwind-css/tailwind.config.js
+++ using-tailwind-css/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        red : "#f63232",
+        white : "#ffffff",
+        mustard : "#feb614",
+        navBg : "#f4f4f4",
+        liColor : "#a0a6b4",
+        liNthChild : "#f64f6f",
+        heading2: "#f64347",
+        paragraph1 : "#55585a",
+        paragraph2 : "#afb6c4",
+        footerH3 : "#4b4b4b"
+      }
+    },
   },
   plugins: [],
 }
